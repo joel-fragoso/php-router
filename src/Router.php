f56ba90a13c86b78893fc087ef25a0e467ff85a1
Joel Fragoso <joelfragoso85@icloud.com>
2020-10-09 11:13:10 -0300

    chore: add license

--- src/Router.php
+++ src/Router.php
@@ -4,31 +4,48 @@ namespace Prix;
 
 use Closure;
 use Prix\Exception\InvalidArgumentException;
+use call_user_func;
 
 final class Router
 {
 	private $routes = [];
 
-	public function addRoute(string $id, Closure $callback)
+	/**
+	 * @var string $id
+	 * @var Closure $callback
+   *
+	 * @return void
+	 */
+	public function addRoute(string $id, Closure $callback): void
 	{
 		$id = $this->parserId($id);
 
 		$this->routes[$id] = $callback;
 	}
 
+	/**
+	 * @var string $id
+	 *
+	 * @return mixed
+	 */
 	public function dispatch(string $id)
 	{
+		$id = $this->parserId($id);
+
 		if (!isset($this->routes[$id])) {
 			throw new InvalidArgumentException("The route ({$id}) does not exist", 400);
 		}
 
-		$id = $this->parserId($id);
-
 		$callback = $this->routes[$id];
 
-		$callback();
+		call_user_func($callback);
 	}
 
+	/**
+	 * @var string $id
+	 *
+	 * @return string
+	 */
 	private function parserId(string $id): string
 	{
 		return '/' . trim($id, '/');
